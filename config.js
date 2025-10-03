4fd6925830d1fd97b007fa51c0f067503f856952
DominicDev <s0l0estudi4r@gmail.com>
2025-10-03 15:42:11 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HOT CHIP';
+export let nameSongs = [
+	"READY FOR THE FLOOR", /*1*/
+	"OVER AND OVER", /*2*/
+	"HITTIN' SKITTLES", /*3*/
+	"NEED YOU NOW", /*4*/
+	"HUARACHE LIGHTS", /*5*/
+	"I FEEL BETTER", /*6*/
+	"NIGHT AND DAY", /*7*/
+	"MELODY OF LOVE", /*8*/
+	"TAKE IT IN", /*9*/
+	"DANCING IN THE DARK", /*10*/
+	"POSITIVE", /*11*/
+	"DOWN", /*12*/
+	"ELEANOR", /*13*/
+	"TIME", /*14*/
+	"BOY FROM SCHOOL", /*15*/
+	"ONE LIFE STAND", /*16*/
+	"THE WARNING", /*17*/
+	"ENDS OF THE EARTH", /*18*/
+	"FLUTES", /*19*/
+	"LOOK AT WHERE WE ARE", /*20*/
+]
